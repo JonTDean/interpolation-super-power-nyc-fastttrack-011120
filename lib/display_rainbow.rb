572a90d8cc622cc4de35909798283cebc6a0db29
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 18:01:49 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -9,7 +9,7 @@ def display_rainbow(colors)
   puts "B: #{colors[4]}"
   puts "I: #{colors[5]}"
   puts "V: #{colors[6]}"
-  puts "\n"
+  
 end
 
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
